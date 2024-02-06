0530cbd20590677da5fa6fd32a0a8b250a9eb028
pooleja <pooleja@users.noreply.github.com>
2024-02-06 14:08:23 -0500

    Fix license

--- contracts/Deposits/DepositQueue.sol
+++ contracts/Deposits/DepositQueue.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
--- contracts/Oracle/Binance/IStakedTokenV2.sol
+++ contracts/Oracle/Binance/IStakedTokenV2.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.19;
 
 /// @dev Interface to get exchange rate on wBETH
